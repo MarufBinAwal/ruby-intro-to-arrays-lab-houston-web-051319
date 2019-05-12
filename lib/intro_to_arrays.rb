23727b3d32601cd8971dc732242219085f5a5bb1
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-12 16:18:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+
+my_new_array = []
+
+end
+
+
+my_two_arrays = ["Red", "Blue"]
+
+end
+
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+   my_first_element = taylor_swift[0]
+   
+   my_third_element = taylor_swift[2]
+   
+    my_last_element = taylor_swift[3]
+ 
+end
+ 
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+ 
+   first_country = south_east_asia.first
+   
+   last_country = south_east_asia.last
+ 
+ end
+ 
+ programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+ 
+  length = programming_languages.length
+  
+end
+
+
+
