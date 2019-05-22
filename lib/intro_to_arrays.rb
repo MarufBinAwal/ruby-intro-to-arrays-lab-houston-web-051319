b08306dad05a38f4338e98e8dbafeb835797daad
Maruf <marufbinawal@gmail.com>
2019-05-22 18:00:43 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,47 @@
+ def instantiate_new_array 
+    my_new_array = Array.new
+
+    return my_new_array
+ end
+
+ def array_with_two_elements
+    my_two_array = [1,2]
+
+    return my_two_array
+ end
+
+def first_element(x)
+    return x[0]
+end
+
+def third_element(x)
+    return x[2]
+end
+
+def last_element(x)
+    return x[-1]
+end
+
+def first_element_with_array_methods(south_east_asia)
+      south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+     return south_east_asia.last
+end
+
+def length_of_array(south_east_asia)
+    return south_east_asia.length
+end
+     
+ 
+  
+
+
+
+
+
+
+
+
+
